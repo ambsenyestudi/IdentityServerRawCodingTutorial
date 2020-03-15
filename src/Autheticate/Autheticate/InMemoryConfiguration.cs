082d51545c391214bfb6ac0d1cca8b641936c34a
ambsenyestudi <adrifre7@hotmail.com>
2020-03-15 12:26:22 +0100

    Redirect

---
+++ src/Autheticate/Autheticate/InMemoryConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Autheticate
+{
+    //renamed due to the name clashing with IConfiguration
+    public static class InMemoryConfiguration
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> GetApis() =>
+
+            new List<ApiResource> {
+                new ApiResource("ApiOne"),
+                new ApiResource("ApiTwo")
+            };
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = { new Secret("client_secret".ToSha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "ApiOne" }
+                },
+                new Client
+                {
+                    ClientId = "client_id_mvc",
+                    ClientSecrets = { new Secret("client_secret_mvc".ToSha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:44392/signin-oidc" },
+                    AllowedScopes = { 
+                        "ApiOne" ,
+                        "ApiTwo",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,}
+
+                }
+
+            };
+    }
+}
--- src/Autheticate/MVCClient/Startup.cs
+++ src/Autheticate/MVCClient/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Autheticate
+namespace MVCClient
 {
     public class Startup
     {
@@ -23,19 +23,26 @@ namespace Autheticate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(config=> 
+                {
+                    config.DefaultScheme="Cookie";
+                    config.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookie")
+                .AddOpenIdConnect("oidc", config=> {
+                    config.Authority = "https://localhost:5101/";
+                    config.ClientId = "client_id_mvc";
+                    config.ClientSecret = "client_secret_mvc";
+                    config.SaveTokens = true;
+                    config.ResponseType = "code";
+                });
+
             services.AddControllersWithViews();
-            //check this configuration at endpoint /.well-known/openid-configuration
-            services.AddIdentityServer()
-                .AddInMemoryApiResources(InMemoryConfiguration.GetApis())
-                .AddInMemoryIdentityResources(InMemoryConfiguration.GetIdentityResources())
-                .AddInMemoryClients(InMemoryConfiguration.GetClients())
-                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,26 +53,20 @@ namespace Autheticate
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            */
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseIdentityServer();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-            //app.UseAuthorization();
-            /*
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-            */
         }
     }
 }

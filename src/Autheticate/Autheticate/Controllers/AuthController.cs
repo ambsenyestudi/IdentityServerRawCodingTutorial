c18eed6435e74fe8f2e8fae3764c867242e812d1
ambsenyestudi <adrifre7@hotmail.com>
2020-03-15 19:48:47 +0100

    Register view

--- src/Autheticate/Autheticate/Controllers/AuthController.cs
+++ src/Autheticate/Autheticate/Controllers/AuthController.cs
@@ -7,10 +7,13 @@ namespace Autheticate.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
         private readonly SignInManager<IdentityUser> signInManager;
 
-        public AuthController(SignInManager<IdentityUser> signInManager)
+        public AuthController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
+            this.userManager = userManager;
             this.signInManager = signInManager;
         }
         [HttpGet]
@@ -35,6 +38,34 @@ namespace Autheticate.Controllers
             {
 
             }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            var model = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var user = new IdentityUser(vm.UserName);
+
+            var result = await userManager.CreateAsync(user, vm.Password);
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+                return Redirect(vm.ReturnUrl);
+            }
+
             return View();
         }
     }
